d0966a9eea7484e6cc1eebb16dcc3693cbbac238
GlDarwin <darwinberumen@gmail.com>
2020-02-20 13:15:29 -0800

    add JS

---
+++ app.js
@@ -0,0 +1,6 @@
+var name = prompt('what is your name?');
+document.write('<h1>' + name + '</h1>');
+var hourNow = prompt('what time is now');
+document.write(hourNow);
+var many = prompt('flowers');
+document.write('<h1>' + many + '</h1>'); 
