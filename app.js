2f836ab138c4523c878e741e62cc7f0376f114c0
GlDarwin <darwinberumen@gmail.com>
2020-02-20 13:59:22 -0800

    add greeting

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var name = prompt('what is your name?');
 document.write('<h1>' + name + '</h1>');
+
 var hourNow = prompt('what time is now');
-document.write(hourNow);
+ 
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow >12) {
+    greeting  = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
+
 var many = prompt('flowers');
-document.write('<h1>' + many + '</h1>'); 
+document.write('<h1>' + many + '</h1>');
